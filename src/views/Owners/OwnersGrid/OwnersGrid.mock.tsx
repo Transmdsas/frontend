7892c9c9985b93a11d707c1681bff840dfd39d03
Daniel Gonzalez <daniel.07151996@gmail.com>
2022-11-06 22:11:39 -0500

    Models For Owners

---
+++ src/views/Owners/OwnersGrid/OwnersGrid.mock.tsx
@@ -0,0 +1,19 @@
+import { GridRowsProp } from "@mui/x-data-grid";
+
+
+export const mockRows: GridRowsProp = [
+    {
+      avatar:
+        "https://image.shutterstock.com/image-photo/young-man-asian-smiling-looking-600w-1848509833.jpg",
+      firstName: "Michael",
+      lastName: "Espinosa",
+      documentType: "CC",
+      documentNumber: "123456789",
+      associatedCar: "ABC123",
+      balances: true,
+      advances: false,
+      createdAt: "2025-08-18T03:58:26.305Z",
+      status: 0.4,
+    },
+  ];
+  
